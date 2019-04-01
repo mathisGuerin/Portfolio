35b6b9099d787fc6053cf9556e80159db0aadacf
mathisGuerin <mathis.guerin@idvroom.com>
2019-04-01 17:09:19 +0200

    Begin home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,41 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        portfolio
-      </h1>
-      <h2 class="subtitle">
-        My portfolio
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+  <div class="Home-Container-top">
+    <div class="Home-Presentation">
+      <div>Hi,</div>
+      <div>I'm Mathis</div>
+      <div>front-end developer</div>
     </div>
-  </section>
+    <video class="Home-bgVideo" autoplay muted loop id="myVideo">
+      <source src="../assets/bgVideo.mp4" type="video/mp4">
+    </video>
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
-  }
-}
+
+};
 </script>
 
+
+
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+.Home-Container-top {
+  height: 100vh;
+  position: relative;
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.Home-Presentation {
+  font-size: 50px;
+  padding: 100px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.Home-bgVideo {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  height: 100%;
+  z-index: -1;
 }
 </style>
 
