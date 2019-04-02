39a9d957703d8851d155b331bdabd81b1d6c274a
mathisGuerin <mathis.guerin@idvroom.com>
2019-04-02 15:56:34 +0200

    Continue on home page - Create navigation menu

---
+++ components/Menu.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <li> Accueil </li>
+    <li> A propos </li>
+    <li> Compétences </li>
+    <li> Exprériences </li>
+    <li> Contact </li>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+
+
+<style lang="scss" scoped>
+    li {
+        display: inline-block;
+        list-style: none;
+        padding: 15px 20px;
+        font-size: 17px;
+        cursor: pointer;
+        transition: all ease-in-out 300ms;
+
+        &:hover {
+            background: rgba(0, 0, 0, 0.6);
+        }
+    }
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,92 @@
 <template>
-  <div class="Home-Container-top">
-    <div class="Home-Presentation">
-      <div>Hi,</div>
-      <div>I'm Mathis</div>
-      <div>front-end developer</div>
+  <div class="Portfolio">
+    <div class="Home-Container-top">
+      <div class="Home-Presentation">
+        <h1>Je suis Mathis Guérin</h1>
+        <h2>Développeur front-end</h2>
+      </div>
+      <img src="../assets/bgPhoto.jpeg"/>
+      <!-- <video class="Home-bgVideo" autoplay muted loop id="myVideo">
+        <source src="../assets/bgVideo.mp4" type="video/mp4">
+      </video> -->
+     <Menu class="Menu"></Menu>
     </div>
-    <video class="Home-bgVideo" autoplay muted loop id="myVideo">
-      <source src="../assets/bgVideo.mp4" type="video/mp4">
-    </video>
   </div>
 </template>
 
 <script>
+import Menu from '../components/Menu.vue'
 export default {
+  components: {
+    Menu
+  }
 
 };
 </script>
 
 
 
-<style>
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,700');
+
+.Portfolio {
+  font-family: 'Open Sans', sans-serif;
+  color: white;
+}
+
 .Home-Container-top {
   height: 100vh;
   position: relative;
+  overflow: hidden;
 }
 
 .Home-Presentation {
-  font-size: 50px;
-  padding: 100px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 35%;
+  text-align: center;
+  margin: 0 auto;
+  padding: 30px 0 30px;
+  max-width: 960px;
+  border: 1px solid #fff;
+  background: rgba(0, 0, 0, 0.3);
+
+  h1 {
+    font-size: 80px;
+    line-height: 90px;
+    font-weight: 700;
+    margin: 0px;
+    padding-bottom: 8px;
+    text-transform: uppercase;
+  }
+
+  h2 {
+    font-size: 34px;
+    font-weight: 300;
+  }
 }
 
 .Home-bgVideo {
   position: absolute;
   top: 0;
-  bottom: 0;
-  height: 100%;
+  left: 0;
   z-index: -1;
+  min-width: 100%; 
+  min-height: 100%; 
+  height: 100%;
+  width: auto; 
+  object-fit: cover;
+}
+
+.Menu {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.5);
+  color: white;
+  text-align: center;
 }
 </style>
 
