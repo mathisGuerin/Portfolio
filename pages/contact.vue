cd7eccff678d1d8196a36f5d8461d0e20e43bf6e
mathisGuerin <mathis.guerin@idvroom.com>
2019-04-03 15:25:54 +0200

    Begin Contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,3 +1,38 @@
 <template>
-    <div>Contact</div>
+    <div class="Contact">
+        <h1>Contact</h1>
+        <div class="Contact-imgs">
+            <a href="https://www.linkedin.com/in/mathis-gu%C3%A9rin-086939100/" target="_blank">
+                <img class="Contact-img" src="../assets/linkedin.png" />
+            </a>
+            <a href="https://github.com/mathisGuerin" target="_blank">
+                <img class="Contact-img" src="../assets/github.svg" />
+            </a>
+            <a href="https://twitter.com/MathisGuerin" target="_blank">
+                <img class="Contact-img" src="../assets/twitter.png" />
+            </a>
+        </div>
+        <div>
+            <h2>Mathis Guérin</h2>
+            <h2>4 rue Gauvin, 49000 Angers</h2>
+            <h2>06.67.54.43.16</h2>
+            <h2>guerin.mathis@gmail.com</h2>
+        </div>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+.Contact {
+    width: 80%;
+    margin: 0 auto;
+}
+
+.Contact-imgs {
+    text-align: center;
+}
+
+.Contact-img {
+    height: 120px;
+    margin: 20px;
+}
+</style>
