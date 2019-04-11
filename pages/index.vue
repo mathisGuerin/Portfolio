1553d40bc187c0dd6b08a854b0cc94215289b37a
mathisGuerin <mathis.guerin@idvroom.com>
2019-04-11 17:30:48 +0200

    Make Home page responsive

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,17 @@
     <div class="Home-Container-top">
       <div class="Home-Presentation">
         <h1>Je suis Mathis Guérin</h1>
-        <vue-typed-js :typeSpeed="100" :contentType="'null'" :strings="['< Développeur front-end />']">
+        <vue-typed-js
+          :typeSpeed="100"
+          :contentType="'null'"
+          :strings="['< Développeur front-end />']"
+        >
           <h2 class="typing"></h2>
         </vue-typed-js>
       </div>
       <!-- <video class="Home-bgVideo" autoplay muted loop id="myVideo">
         <source src="../assets/bgVideo.mp4" type="video/mp4">
       </video>-->
-
     </div>
   </div>
 </template>
@@ -45,29 +48,40 @@ export default {};
   position: absolute;
   left: 0;
   right: 0;
-  top: 35%;
+  top: 12%;
   text-align: center;
   margin: 0 auto;
   padding: 30px 0 30px;
   border: 1px solid #fff;
   background: rgba(0, 0, 0, 0.8);
 
+  @media screen and (min-width: 600px) {
+    top: 28%;
+  }
+
   @media screen and (min-width: 1000px) {
+    top: 35%;
     max-width: 960px;
   }
 
   h1 {
-    font-size: 80px;
-    line-height: 90px;
+    font-size: 67px;
+    line-height: 74px;
     font-weight: 700;
     margin: 0px;
     padding-bottom: 8px;
     text-transform: uppercase;
+
+    @media screen and (min-width: 600px) {
+      top: 28%;
+      font-size: 80px;
+      line-height: 90px;
+    }
   }
 
   .typed-element {
     justify-content: center;
-  } 
+  }
 
   h2 {
     font-size: 34px;
