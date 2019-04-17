664f6781a6641d8fe717ce60e1d00fa8db830c33
mathisGuerin <mathis.guerin@idvroom.com>
2019-04-17 11:56:29 +0200

    Add responsive menu

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,31 +1,63 @@
 <template>
   <div>
-    <nuxt-link to="/">Accueil</nuxt-link><nuxt-link to="/about">A propos</nuxt-link><nuxt-link to="/skills">Compétences</nuxt-link><nuxt-link to="/experiences">Expériences</nuxt-link><nuxt-link to="/contact">Contact</nuxt-link>
+    <nuxt-link to="/">
+      <span>Accueil</span>
+      <font-awesome-icon class="Menu-icon" icon="home"/>
+    </nuxt-link>
+    <nuxt-link to="/about">
+      <span>A propos</span>
+      <font-awesome-icon class="Menu-icon" icon="user"/>
+    </nuxt-link>
+    <nuxt-link to="/skills">
+      <span>Compétences</span>
+      <font-awesome-icon class="Menu-icon" icon="check-circle"/>
+    </nuxt-link>
+    <nuxt-link to="/experiences">
+      <span>Expériences</span>
+      <font-awesome-icon class="Menu-icon" icon="laptop"/>
+    </nuxt-link>
+    <nuxt-link to="/contact">
+      <span>Contact</span>
+      <font-awesome-icon class="Menu-icon" icon="at"/>
+    </nuxt-link>
   </div>
 </template>
 
 <script>
-export default {
-
-};
+export default {};
 </script>
 
 
 
 <style lang="scss" scoped>
-    a {
-        display: inline-block;
-        list-style: none;
-        padding: 15px 20px;
-        font-size: 17px;
-        cursor: pointer;
-        transition: all ease-in-out 300ms;
-        color: white;
-        text-decoration: none;
+span {
+  display: none;
+  @media screen and (min-width: 700px) {
+    display: inline;
+  }
+}
 
-        &:hover {
-            background: rgba(0, 0, 0, 0.7);
-        }
-    }
+.Menu-icon {
+  width: 20px;
+  height: 20px;
+  @media screen and (min-width: 700px) {
+    display: none;
+  }
+}
+
+a {
+  display: inline-block;
+  list-style: none;
+  padding: 15px 20px;
+  font-size: 17px;
+  cursor: pointer;
+  transition: all ease-in-out 300ms;
+  color: white;
+  text-decoration: none;
+
+  &:hover, &.nuxt-link-exact-active {
+    background: rgba(0, 0, 0, 0.7);
+  }
+}
 </style>
 
