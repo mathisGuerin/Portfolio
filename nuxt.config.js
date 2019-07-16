2d497ab87ab4d2e8a8679663d945233a8f3a50b9
mathisGuerin <mathis.guerin@idvroom.com>
2019-07-16 10:52:24 +0200

    Add google verification

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,46 +1,50 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'Mathis Guérin',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Portfolio Mathis Guérin - Développeur front-end' }
+      {
+        name: 'google-site-verification',
+        content: 'LI_z_1xaBLzMq5XAi',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Portfolio Mathis Guérin - Développeur front-end',
+      },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   /*
-  ** Customize the progress bar color
-  */
+   ** Customize the progress bar color
+   */
   loading: false,
   css: [
     '@fortawesome/fontawesome-svg-core/styles.css',
-    '~/assets/css/main.css'
+    '~/assets/css/main.css',
   ],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
+     ** Run ESLint on save
+     */
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
+          exclude: /(node_modules)/,
+        });
       }
-    }
+    },
   },
-  plugins: [
-    '~/plugins/fontawesome.js'
-  ]
-}
+  plugins: ['~/plugins/fontawesome.js'],
+};
 
